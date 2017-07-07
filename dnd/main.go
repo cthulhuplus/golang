253a093324d63239932893038f8592bb8ec42ed0
cthulhuplus <brookswm@gmail.com>
2017-07-07 14:36:24 -0400

    First commit

---
+++ dnd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func random(min, max int) int {
+  rand.Seed(time.Now().Unix())
+  return rand.Intn(max-min) + min
+}
+
+func main() {
+  fourSide := random(1, 4)
+  sixSide := random(1, 6)
+  eightSide := random(1, 8)
+  tenSide := random(1, 10)
+//  percSide := // TODO
+  twelveSide := random(1, 12)
+  twentySide := random(1, 20)
+  fmt.Println("D4  Roll:", fourSide)
+  fmt.Println("D6  Roll:", sixSide)
+  fmt.Println("D8  Roll:", eightSide)
+  fmt.Println("D10 Roll:", tenSide)
+  fmt.Println("D12 Roll:", twelveSide)
+  fmt.Println("D20 Roll:", twentySide)
+}
