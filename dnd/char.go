82204aec4eb13ff31efed868556b69fdb8cb499a
cthulhuplus <brookswm@gmail.com>
2017-07-11 08:53:43 -0400

    Started character struc

---
+++ dnd/char.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+)
+
+// Creating the struc for Character attributes/stats
+
+type Char struct {
+  Name string
+  Race string
+  Sub string
+  Str int
+  Dex int
+  Wis int
+  Int int
+  Cha int
+  Con int
+}
