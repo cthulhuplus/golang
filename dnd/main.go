593a4ab064a58280c419d2b5791a4324d7a71d74
cthulhuplus <brookswm@gmail.com>
2017-08-15 21:52:59 -0400

    Started attribute calculation

--- dnd/main.go
+++ dnd/main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-  "fmt"
-//  "math/rand"
-//  "time"
-//  "."
+	"fmt"
+	"math/rand"
+	"time"
+	//  "."
 )
 
-//func random(min, max int) int {
-//  rand.Seed(time.Now().Unix())
-//  return rand.Intn(max-min) + min
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+//func attributes() {
+//	str := fourSide x 6
+//	var str []int
+//	str = append(str, sixSide)
 //}
 
 func main() {
-  fourSide := random(1, 4)
-  sixSide := random(1, 6)
-  eightSide := random(1, 8)
-  tenSide := random(1, 10)
-//  percSide := // TODO
-  twelveSide := random(1, 12)
-  twentySide := random(1, 20)
-  fmt.Println("D4  Roll:", fourSide)
-  fmt.Println("D6  Roll:", sixSide)
-  fmt.Println("D8  Roll:", eightSide)
-  fmt.Println("D10 Roll:", tenSide)
-  fmt.Println("D12 Roll:", twelveSide)
-  fmt.Println("D20 Roll:", twentySide)
+	fourSide := random(1, 4)
+	sixSide := random(1, 6)
+	eightSide := random(1, 8)
+	tenSide := random(1, 10)
+	//  percSide := // TODO
+	twelveSide := random(1, 12)
+	twentySide := random(1, 20)
+
+	var str []int
+
+	str = append(str, sixSide)
+
+	fmt.Println("D4  Roll:", fourSide)
+	fmt.Println("D6  Roll:", sixSide)
+	fmt.Println("D8  Roll:", eightSide)
+	fmt.Println("D10 Roll:", tenSide)
+	fmt.Println("D12 Roll:", twelveSide)
+	fmt.Println("D20 Roll:", twentySide)
+
+	fmt.Println("Str:", str)
 }
